774ca5d5d194b1986cae4a4bf4cdb0468634587d
b45i <amalshajan2011@gmail.com>
2021-11-27 20:39:20 +0530

    s3 migrate

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 const db = require('./models/image.model');
+const axios = require('axios');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.ACCESS_KEY,
+});
+
+const bucketName = process.env.BUCKET_NAME;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
@@ -13,14 +22,32 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 });
 
 // dont use in prod
-sequelize.sync().then(() => {
-    console.log('Drop and re-sync db.');
-});
+// sequelize.sync().then(() => {
+//     console.log('Drop and re-sync db.');
+// });
 
 const Images = db(sequelize);
 
-const getAllImages = async () => {
-    const data = await Images.findAll();
-};
+async function migrateImages() {
+    const results = await Images.findAll();
 
-getAllImages();
+    const uploadPromises = results.map(async ({ dataValues }) => {
+        const { name, url } = dataValues;
+
+        console.log(`Downloading ${name}`);
+        const response = await axios(url, { responseType: 'arraybuffer' });
+
+        const data = await s3
+            .putObject({
+                Bucket: bucketName,
+                Key: name,
+                Body: response.data,
+            })
+            .promise();
+        console.log(`${name} Migrated to S3`);
+    });
+
+    Promise.all(uploadPromises);
+}
+
+migrateImages();
